86d9561ac287e27dc96b7237649806e99565100a
Balazs Kereszturi <19balazs86@gmail.com>
2024-06-20 20:27:24 +0200

    Apply: ResilienceHandler

--- ClientConsoleApp/HelloClientExtensions.cs
+++ ClientConsoleApp/HelloClientExtensions.cs
@@ -1,8 +1,6 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Http.Resilience;
 using Polly;
-using Polly.Extensions.Http;
-using Polly.Retry;
-using Polly.Timeout;
 
 namespace ClientConsoleApp;
 
@@ -12,19 +10,27 @@ public static class HelloClientExtensions
 
     public static IServiceCollection AddHelloClient(this IServiceCollection services)
     {
-        // --> Create: Polly policy.
-        AsyncRetryPolicy<HttpResponseMessage> retryPolicy = HttpPolicyExtensions
-            .HandleTransientHttpError()
-            .Or<TimeoutRejectedException>() // Thrown by Polly's TimeoutPolicy if the inner call gets timeout.
-            .WaitAndRetryAsync(2, _ => TimeSpan.FromMilliseconds(500));
-
-        AsyncTimeoutPolicy<HttpResponseMessage> timeoutPolicy = Policy
-            .TimeoutAsync<HttpResponseMessage>(TimeSpan.FromMilliseconds(500));
-
-        services.AddHttpClient<IHelloClient, HelloClient>(httpClient => httpClient.BaseAddress = _webApiUri)
-            .AddPolicyHandler(retryPolicy)
-            .AddPolicyHandler(timeoutPolicy);
+        services
+            .AddHttpClient<IHelloClient, HelloClient>(httpClient => httpClient.BaseAddress = _webApiUri)
+            .AddResilienceHandler("user-pipeline", configureResilienceHandler);
 
         return services;
+    }
+
+    private static void configureResilienceHandler(ResiliencePipelineBuilder<HttpResponseMessage> pipelineBuilder)
+    {
+        // --> Define option: Retry
+        var retryOptions = new HttpRetryStrategyOptions
+        {
+            MaxRetryAttempts = 2,
+            Delay            = TimeSpan.FromMilliseconds(500),
+            BackoffType      = DelayBackoffType.Constant,
+        };
+
+        // --> Configure: Pipeline
+        pipelineBuilder
+            .AddTimeout(TimeSpan.FromSeconds(5)) // Total timeout for the request execution
+            .AddRetry(retryOptions)
+            .AddTimeout(TimeSpan.FromMilliseconds(500)); // Timeout per each request attempt
     }
 }
--- WebApi/Dummy.cs
+++ WebApi/Dummy.cs
@@ -6,8 +6,9 @@ public static class Dummy
 {
     public static readonly IReadOnlyList<HttpStatusCode> StatusCodes =
     [
-        HttpStatusCode.BadRequest,  // Polly won't retry for this.
-        HttpStatusCode.NotFound,    // Polly won't retry for this.
+        HttpStatusCode.BadRequest,           // Polly won't retry for this.
+        HttpStatusCode.NotFound,             // Polly won't retry for this.
+        HttpStatusCode.UnprocessableContent, // Polly won't retry for this
         HttpStatusCode.RequestTimeout,
         HttpStatusCode.RequestTimeout,
         HttpStatusCode.InternalServerError,
